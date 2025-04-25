b776772457c3b5fe4556d2ab5c263d8fc31ad207
pilisir <pilisir.tw@gmail.com>
2025-04-25 20:43:27 +0800

    Modify PersonTest.java from GitHub cloud

--- src/test/java/com/mywebsb/entity/PersonTest.java
+++ src/test/java/com/mywebsb/entity/PersonTest.java
@@ -42,7 +42,7 @@ class PersonTest {
 		Mockito.when(personDao.findFirstPerson()).thenReturn(p);
 		String actual = personDao.findFirstPerson().getFirstName();
 //		String actual = welcomeService.getFirstPersonFirstName();
-		String expect = "傑森1";
+		String expect = "傑森2";
 
 		Assertions.assertNotNull(actual);
 		Assertions.assertEquals(expect, actual);
